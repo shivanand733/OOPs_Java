09ec31c8a86f57dd1d52382a6f0d3aed3ce6931b
shivanand733 <shivanandkumbar5604@gmail.com>
2025-10-24 10:12:41 +0530

    check to how to work the default constructor

---
+++ src/constructor/shiv.java
@@ -0,0 +1,15 @@
+package constructor;
+
+public class shiv {
+
+	int id;
+	String firstName;
+	String lastName;
+	
+	public shiv(int id,String firstName,String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+}
