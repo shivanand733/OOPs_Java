39cdc768b84927eb4cbcd91f3c3fba2d18d8543a
shivanand733 <shivanandkumbar5604@gmail.com>
2025-11-13 15:51:09 +0530

    Done with coderesusability , flexible and easy to maintanable , easy to use.....

--- src/Agreegation/planeApp.java
+++ src/Agreegation/planeApp.java
@@ -2,10 +2,8 @@ package Agreegation;
 
 public class planeApp {
 public static void main(String[] args) {
-	cargoPlane cp = new cargoPlane();
-	flay.permit(cp);
-	passangerPlane pp = new passangerPlane();
-	flay.permit(pp);
+ 	flay.permit(new cargoPlane());
+ 	flay.permit(new passangerPlane());
 
 }
 }
