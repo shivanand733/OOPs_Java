4781849c4618ce4a3789ce82a9ffa75e8b6b6e8b
shivanand733 <shivanandkumbar5604@gmail.com>
2025-11-14 13:32:15 +0530

    create the abstraction class

---
+++ src/AbstractClasses/Bus.java
@@ -0,0 +1,34 @@
+package AbstractClasses;
+
+abstract public class Bus {
+	private int busId;
+	private String busName;
+	
+	public Bus() {
+		//default constructor 
+	}
+	
+	//parametrized constructor 
+	public Bus(int busId, String busName) {
+		this.busId = busId;
+		this.busName = busName;
+	}
+
+	
+	//setters and getters
+	public int getBusId() {
+		return busId;
+	}
+	
+	public String getBusName() {
+		return busName;
+	}
+	
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
+	
+	public void setBusName(String busName) {
+		this.busName = busName;
+	}
+}
