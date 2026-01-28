2602ec5593647371185fdf4d680fe5b191073bf7
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-28 22:24:01 +0530

    crate a simple inner class

---
+++ src/FInal/helloShivu.java
@@ -0,0 +1,10 @@
+package FInal;
+
+public class helloShivu {
+    public void Shivu() {
+        System.out.println("This is the call the one class to another class ");
+    }
+public static void main(String[] args) {
+    
+}
+}
--- src/interfaceJava/Demo1.java
+++ src/interfaceJava/Demo1.java
@@ -1,6 +1,7 @@
 package interfaceJava;
 
 public interface Demo1 {
-
+System.out.println("hello");
 	public void play();
+
 }
