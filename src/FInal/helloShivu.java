e49bd0e0fe49614c5b26276cd1daab0438a55e94
shivanand733 <shivanandkumbar5604@gmail.com>
2026-01-30 19:51:48 +0530

    create a simple changes in the program

--- src/FInal/helloShivu.java
+++ src/FInal/helloShivu.java
@@ -4,9 +4,13 @@ public class helloShivu {
     public void Shivu() {
         System.out.println("This is the call the one class to another class ");
     }
+    public void prashanth() {
+        System.out.println("The call another methode ....");
+    }
 public static void main(String[] args) {
 
     helloShivu hello = new helloShivu();
     hello.Shivu();
+    hello.prashanth();
 }
 }
